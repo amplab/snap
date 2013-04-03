0904ceee0f72a42d23bf392d79642567d1b647a9
magis1 <magis1@sysbio.scs.uiuc.edu>
2013-04-03 15:39:01 -0500

    Added RNASeq aligning capability

---
+++ SNAPLib/GTFReader.cpp
@@ -0,0 +1,294 @@
+//Source includes
+#include "GTFReader.h"
+
+//System includes
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+#include "Compat.h"
+
+GTFFeature::GTFFeature(string line) {
+
+    char _chr[GTF_MAX_READ_SIZE];
+    char _source[GTF_MAX_READ_SIZE];
+    char _feature[GTF_MAX_READ_SIZE];
+    unsigned _start;
+    unsigned _end;
+    char _score[GTF_MAX_READ_SIZE];
+    char _strand;
+    char _frame;
+    char _gene_id[GTF_MAX_READ_SIZE];
+    char _transcript_id[GTF_MAX_READ_SIZE];
+    
+    //Tried stringstream - it is horrible, not thread safe, incredibly slow in a multithreaded application
+    sscanf(line.c_str(), "%s\t%s\t%s\t%u\t%u\t%s\t%c\t%c\t%*[^\"]\"%255[^\"]\"%*[^\"]\"%255[^\"]\"", _chr, _source, _feature, &_start, &_end, _score, &_strand, &_frame, &_gene_id, &_transcript_id);
+        
+    chr = _chr;
+    source = _source;
+    feature = _feature;
+    start = _start;
+    end = _end;
+    score = _score;
+    strand = _strand;
+    frame = _frame;
+    gene_id = _gene_id;
+    transcript_id = _transcript_id;
+    
+}
+
+unsigned GTFFeature::Length() const {
+    return (end-start)+1;
+}
+
+void GTFFeature::Print() const {
+    printf("%s\t%s\t%s\t%u\t%u\t%s\t%c\t%c\t%s\t%s\n", chr.c_str(), source.c_str(), feature.c_str(), start, end, score.c_str(), strand, frame, gene_id.c_str(), transcript_id.c_str());
+}
+
+GTFFeature::GTFFeature(const GTFFeature& rhs) 
+    :   chr(rhs.chr), source(rhs.source), feature(rhs.feature), start(rhs.start), end(rhs.end), score(rhs.score),
+        strand(rhs.strand), frame(rhs.frame), gene_id(rhs.gene_id), transcript_id(rhs.transcript_id)
+{}
+  
+GTFFeature::~GTFFeature() {}
+
+GTFFeature& GTFFeature::operator=(const GTFFeature& rhs) {
+
+    if (this != &rhs) {
+        chr = rhs.chr;
+        source = rhs.source;
+        feature = rhs.feature;
+        start = rhs.start;
+        end = rhs.end;
+        score = rhs.score;
+        strand = rhs.strand;
+        frame = rhs.frame;
+        gene_id = rhs.gene_id;
+        transcript_id = rhs.transcript_id;
+    }
+    return *this;
+}
+
+bool GTFFeature::operator<(const GTFFeature& rhs) const {
+    return start < rhs.start;
+}
+
+GTFTranscript::GTFTranscript(string _chr, string _gene_id, string _transcript_id) 
+    : chr(_chr), gene_id(_gene_id), transcript_id(_transcript_id) 
+{}
+
+GTFTranscript::GTFTranscript(const GTFTranscript& rhs) 
+    : chr(rhs.chr), gene_id(rhs.gene_id), transcript_id(rhs.transcript_id), features(rhs.features)
+{}
+  
+GTFTranscript::~GTFTranscript() {}
+
+GTFTranscript& GTFTranscript::operator=(const GTFTranscript& rhs) {
+
+    if (this != &rhs) {
+        chr = rhs.chr;
+        gene_id = rhs.gene_id;
+        transcript_id = rhs.transcript_id;
+        features = rhs.features;   
+    }
+    return *this;
+}
+
+void GTFTranscript::Process() {
+
+    //Copy all exons into separate vector
+    for (feature_list::iterator it = features.begin(); it != features.end(); ++it) {
+        if (it->feature.compare("exon") == 0) {
+            exons.push_back(*it);
+        }
+    }
+
+    //Sort the exons by start and end
+    std::sort(exons.begin(), exons.end());
+    
+}
+
+unsigned GTFTranscript::GenomicPosition(unsigned transcript_pos) {
+       
+    //This assumes 1-offset transcript pos, and returns 1-offset genomic position
+    //Converts transcript coordinates into genomic coordinates
+    for (feature_list::iterator it = exons.begin(); it != exons.end(); ++it) {
+        
+        //If transcript_pos is less than or equal to this
+        if (transcript_pos > it->Length()) {
+            transcript_pos -= it->Length();
+        } else {
+            return it->start + transcript_pos - 1;
+        }
+    }
+    printf("Warning, transcript_pos exceeds transcript length\n");
+    return 0;
+}
+
+std::vector<junction> GTFTranscript::Junctions(unsigned transcript_pos, unsigned span) {
+
+    //Container for splice junctions
+    std::vector<junction> junctions;
+    
+    //Go through each feature of this transcript until we find the start
+    unsigned current_pos = 0;
+    feature_list::iterator current = exons.begin();
+    for (feature_list::iterator next = ++(exons.begin()); next != exons.end(); ++next) {
+
+        //Get the end of this exon
+        current_pos += current->Length();
+
+        //If transcript_pos is less than or equal to current, the start
+        //lies within this feature
+        if (transcript_pos <= current_pos) {
+
+            //Check to see if the span exceeds the current feature. If so, 
+            //simply return the current list of junctions
+            if (current_pos-transcript_pos+1 >= span) {
+                return junctions;
+                
+            //Otherwise, add the junction to the list of junctions
+            } else {
+            
+                junctions.push_back(junction(current_pos+1, next->start-current->end-1));
+                span -= current_pos-transcript_pos+1;
+            
+            }
+        }
+        current = next;            
+    }
+    return junctions;
+
+}
+
+//Constructor
+GTFReader::GTFReader() {}
+
+//Destructor
+GTFReader::~GTFReader() {}
+
+int GTFReader::Load(string _filename) {
+
+    //Save the input filename
+    filename = _filename;
+    
+    printf("Loading genome annotation from file... ");
+    fflush(stdout);
+    _int64 loadStart = timeInMillis();
+    
+    //Open input file
+    std::ifstream infile(filename.c_str(), std::ios::in);
+    if (!infile.is_open()) { 
+        printf("Cannot open input file '%s'\n", filename.c_str());
+	    exit(1);
+    }
+    
+    string line;
+    unsigned num_lines = 0;
+    std::getline(infile, line, '\n');
+    while (!infile.eof()){
+  
+        //Process this line
+        Parse(line);
+        num_lines++;
+          
+        std::getline(infile, line, '\n');
+    }
+    infile.close();
+    
+    //Sort each transcript
+    for (transcript_map::iterator it = transcripts.begin(); it != transcripts.end(); ++it) {
+        (*it).second.Process();
+    }
+    
+    _int64 loadTime = timeInMillis() - loadStart;
+    printf("%llds.  %u features,  %u transcripts\n", loadTime / 1000, num_lines, transcripts.size());
+    
+}
+
+int GTFReader::Parse(string line) {
+
+    // Create a new GTFFeature from this line
+    GTFFeature feature(line);
+    
+    //Try to find this transcript in the transcript_map
+    transcript_map::iterator pos = transcripts.find(feature.transcript_id);
+        
+    //If this sequence is not found, create a new vector to store this sequence (and others like it)
+    if ((pos == transcripts.end())) {
+        GTFTranscript transcript(feature.chr, feature.gene_id, feature.transcript_id);
+        transcript.features.push_back(feature);
+        transcripts.insert(transcript_map::value_type(feature.transcript_id, transcript));
+        
+    //Otherwise, add this feature to the transcript
+    } else {
+        pos->second.features.push_back(feature);
+    }
+    return 0;
+    
+}
+
+GTFTranscript& GTFReader::GetTranscript(string transcript_id) {
+ 
+    transcript_map::iterator pos = transcripts.find(transcript_id);
+    if (pos == transcripts.end()) {
+        //raise exception
+        printf("No transcript %s\n", transcript_id.c_str());
+        exit(1);
+    }
+    return pos->second;
+
+}
+
+void GTFReader::Test() {
+
+    GTFTranscript &transcript = GetTranscript("ENST00000489673");
+
+    std::vector<junction> junctions = transcript.Junctions(1, 200);
+    
+    for (std::vector<junction>::iterator it = junctions.begin(); it != junctions.end(); ++it) {
+        printf("[%d %d]\n", it->first, it->second);
+    }
+
+
+}
+
+/*
+int main(int argc, char *argv[]) {
+
+    const char *filename = NULL;
+
+    //Process the command-line arguments
+    int c;
+    while ((c=getopt(argc, argv, "f:")) != EOF) {
+    
+        switch(c) {
+            case 'f':
+                filename = optarg;
+                break;
+            case ':':
+                cerr << "Invalid option " << optarg << endl;
+                exit(1);
+                break; 	
+	    }
+    }
+    
+    struct timeval t1;
+    gettimeofday(&t1, NULL);
+    
+    //Create the gtfReader
+    GTFReader gtf;
+    gtf.Load(filename);
+    gtf.Test();
+    
+    struct timeval t2;
+    gettimeofday(&t2, NULL);
+    
+    double elapsedTime;
+    elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;
+    elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;
+    fprintf(stderr, "Time elapsed: %.3lf seconds\n", elapsedTime / 1000.0);
+    return 0;
+}
+*/
+
---
+++ SNAPLib/RNASeqAligner.cpp
@@ -0,0 +1,461 @@
+/*++
+
+Module Name:
+
+    RNASeqAligner.cpp
+
+Abstract:
+
+    Functions for running the paired end aligner sub-program.
+
+
+Authors:
+
+    Andrew Magis, March 2013
+
+    Adapted from PairedAligner.cpp, Matei Zaharia
+
+Environment:
+`
+    User mode service.
+
+Revision History:
+
+    Adapted from cSNAP, which was in turn adapted from the scala prototype
+
+--*/
+
+//
+// TODO: This is really similar to the single-end aligner overall. It would be nice
+// to avoid code duplication.
+//
+
+#include "stdafx.h"
+#include "options.h"
+#include <time.h>
+#include "Compat.h"
+#include "RangeSplitter.h"
+#include "GenomeIndex.h"
+#include "Range.h"
+#include "SAM.h"
+#include "RNASeqPairedEndAligner.h"
+#include "Tables.h"
+#include "WGsim.h"
+#include "GoodRandom.h"
+#include "AlignerOptions.h"
+#include "AlignerContext.h"
+#include "AlignerStats.h"
+#include "FASTQ.h"
+#include "RNASeqAligner.h"
+
+using namespace std;
+
+static const int DEFAULT_MIN_SPACING = 100;
+static const int DEFAULT_MAX_SPACING = 1000;
+
+struct RNASeqAlignerStats : public AlignerStats
+{
+    // TODO: make these constants configurable
+    static const int MAX_DISTANCE = 1000;
+    static const int MAX_SCORE = 15;
+
+    _int64 sameComplement;
+    _int64* distanceCounts; // histogram of distances
+    // TODO: could save a bit of memory & time since this is a triangular matrix
+    _int64* scoreCounts; // 2-d histogram of scores for paired ends
+
+    RNASeqAlignerStats(AbstractStats* i_extra = NULL);
+
+    virtual ~RNASeqAlignerStats();
+
+    inline void incrementDistance(int distance) {
+        distanceCounts[max(0, min(MAX_DISTANCE, distance))]++;
+    }
+
+    inline void incrementScore(int s0, int s1)
+    {
+        // ensure s0 <= s1, both within range
+        s0 = max(0, min(MAX_SCORE, s0));
+        s1 = max(0, min(MAX_SCORE, s1));
+        if (s0 > s1) {
+            int t = s0; s0 = s1; s1 = t;
+        }
+        scoreCounts[s0*(MAX_SCORE+1)+s1]++;
+    }
+
+    virtual void add(AbstractStats * other);
+
+    virtual void printHistograms(FILE* output);
+};
+
+const int RNASeqAlignerStats::MAX_DISTANCE;
+const int RNASeqAlignerStats::MAX_SCORE;
+
+RNASeqAlignerStats::RNASeqAlignerStats(AbstractStats* i_extra)
+    : AlignerStats(i_extra),
+    sameComplement(0)
+{
+    int dsize = sizeof(_int64) * (MAX_DISTANCE+1);
+    distanceCounts = (_int64*)BigAlloc(dsize);
+    memset(distanceCounts, 0, dsize);
+
+    int ssize = sizeof(_int64) * (MAX_SCORE+1)*(MAX_SCORE+1);
+    scoreCounts = (_int64*)BigAlloc(ssize);
+    memset(scoreCounts, 0, ssize);
+}
+
+RNASeqAlignerStats::~RNASeqAlignerStats()
+{
+    BigDealloc(distanceCounts);
+    BigDealloc(scoreCounts);
+}
+
+void RNASeqAlignerStats::add(AbstractStats * i_other)
+{
+    AlignerStats::add(i_other);
+    RNASeqAlignerStats* other = (RNASeqAlignerStats*) i_other;
+    for (int i = 0; i < MAX_DISTANCE + 1; i++) {
+        distanceCounts[i] += other->distanceCounts[i];
+    }
+    for (int i = 0; i < (MAX_SCORE + 1) * (MAX_SCORE + 1); i++) {
+        scoreCounts[i] += other->scoreCounts[i];
+    }
+}
+
+void RNASeqAlignerStats::printHistograms(FILE* output)
+{
+    AlignerStats::printHistograms(output);
+    // print all non-zeros
+    fprintf(output, "\ndistance\tpairs\n");
+    for (int i = 0; i <= MAX_DISTANCE; i++) {
+        if (distanceCounts[i] != 0) {
+            fprintf(output, "%d\t%lld\n", i, distanceCounts[i]);
+        }
+    }
+
+    fprintf(output, "\nscores\n");
+    int max1 = MAX_SCORE;
+    bool found = false;
+    while (max1 > 0) {
+        for (int i = 0; i <= max1 && !found; i++) {
+            found = scoreCounts[i * (MAX_SCORE + 1) + max1] != 0;
+        }
+        if (found) {
+            break;
+        }
+        max1--;
+    }
+    for (int s1 = 0; s1 <= max1; s1++) {
+        for (int s0 = 0; s0 <= s1; s0++) {
+            fprintf(output, "%lld%s", scoreCounts[s0 * (MAX_SCORE + 1) + s1], s0 < s1 ? "\t" : "\n");
+        }
+    }
+}
+
+static bool readIdsMatch(Read *read0, Read *read1)
+{
+    if (read0->getIdLength() != read1->getIdLength()) {
+        return false;
+    }
+    for (unsigned i = 0; i < read0->getIdLength(); i++) {
+        char c0 = read0->getId()[i];
+        char c1 = read1->getId()[i];
+        if (c0 != c1 && !(c0 == '1' && c1 == '2')) {
+            return false;
+        }
+    }
+    return true;
+}
+
+RNASeqAlignerOptions::RNASeqAlignerOptions(const char* i_commandLine)
+    : AlignerOptions(i_commandLine, true),
+    minSpacing(DEFAULT_MIN_SPACING),
+    maxSpacing(DEFAULT_MAX_SPACING)
+{
+}
+
+void RNASeqAlignerOptions::usageMessage()
+{
+    AlignerOptions::usageMessage();
+    printf(
+        "  -s   min and max spacing to allow between paired ends (default: %d %d)\n",
+        DEFAULT_MIN_SPACING,
+        DEFAULT_MAX_SPACING);
+}
+
+bool RNASeqAlignerOptions::parse(const char** argv, int argc, int& n)
+{
+    if (strcmp(argv[n], "-s") == 0) {
+        if (n + 2 < argc) {
+            minSpacing = atoi(argv[n+1]);
+            maxSpacing = atoi(argv[n+2]);
+            n += 2;
+            return true;
+        } 
+        return false;
+    }
+    return AlignerOptions::parse(argv, argc, n);
+}
+
+RNASeqAlignerContext::RNASeqAlignerContext(AlignerExtension* i_extension)
+    : AlignerContext( 0,  NULL, NULL, i_extension)
+{
+}
+
+// Check whether a string str ends with a given pattern
+static bool stringEndsWith(const char* str, const char* pattern) {
+    if (strlen(str) < strlen(pattern)) {
+        return false;
+    } else {
+        return strcmp(str + (strlen(str) - strlen(pattern)), pattern) == 0;
+    }
+}
+
+AlignerOptions* RNASeqAlignerContext::parseOptions(int i_argc, const char **i_argv, const char *i_version)
+{
+    argc = i_argc;
+    argv = i_argv;
+    version = i_version;
+
+    RNASeqAlignerOptions* options = new RNASeqAlignerOptions(
+        "snap rnaseq <genome-dir> <transcriptome-dir> <gtf-annotation> <read1.fq> <read2.fq> [-o output.sam] [<options>]\n"
+        "   or  snap paired <index-dir> <reads.sam> [-o output.sam] [<options>]");
+    options->extra = extension->extraOptions();
+    
+    if (argc < 2) {
+        options->usage();
+    }
+
+    options->indexDir = argv[0];
+    options->transcriptomeDir = argv[1];
+    options->annotation = argv[2];
+    options->inputFilename = argv[3];
+
+    bool samInput = stringEndsWith(argv[3], ".sam");
+
+    if (argc < 5 && !samInput) {
+        options->usage();
+    }
+
+	options->inputFileIsFASTQ = !samInput;
+    options->fastqFile1 = samInput ? NULL : argv[4];
+
+    for (int n = (samInput ? 4 : 5); n < argc; n++) {
+        if (!options->parse(argv, argc, n)) {
+            options->usage();
+        }
+    }
+    
+    // Sanity check to make sure our parallel splitting will work.
+    if (options->inputFileIsFASTQ && QueryFileSize(options->inputFilename) != QueryFileSize(options->fastqFile1)) {
+        fprintf(stderr, "The two FASTQ files are not the same size! Make sure they contain\n"
+                        "the same reads in the same order, without comments.\n");
+#if     USE_DEVTEAM_OPTIONS
+        fprintf(stderr,"DEVTEAM: Allowing run, but you need to run on only one thread because the file splitter\n");
+        fprintf(stderr,"Doesn't understand how to deal with files that don't match byte-for-byte.\n");
+        options->numThreads = 1;
+#else   // USE_DEVTEAM_OPTIONS
+        exit(1);
+#endif  // USE_DEVTEAM_OPTIONS
+    }
+
+    return options;
+}
+
+void RNASeqAlignerContext::initialize()
+{
+    AlignerContext::initialize();
+    RNASeqAlignerOptions* options2 = (RNASeqAlignerOptions*) options;
+    minSpacing = options2->minSpacing;
+    maxSpacing = options2->maxSpacing;
+    fastqFile1 = options2->fastqFile1;
+    ignoreMismatchedIDs = options2->ignoreMismatchedIDs;
+    
+    printf("Loading transcriptome index from directory... ");
+    fflush(stdout);
+    _int64 loadStart = timeInMillis();
+    transcriptome = GenomeIndex::loadFromDirectory((char*) options->transcriptomeDir);
+    if (index == NULL) {
+        fprintf(stderr, "Transcriptome load failed, aborting.\n");
+        exit(1);
+    }
+        
+    _int64 loadTime = timeInMillis() - loadStart;
+    printf("%llds.  %u bases, seed size %d\n",
+        loadTime / 1000, transcriptome->getGenome()->getCountOfBases(), transcriptome->getSeedLength());
+    
+    //Load the GTF file
+    gtf->Load(options->annotation);
+}
+
+AlignerStats* RNASeqAlignerContext::newStats()
+{
+    return new RNASeqAlignerStats();
+}
+
+void RNASeqAlignerContext::runTask()
+{
+    ParallelTask<RNASeqAlignerContext> task(this);
+    task.run();
+}
+
+void RNASeqAlignerContext::runIterationThread()
+{
+    int maxReadSize = 10000;
+    RNASeqPairedEndAligner *aligner = new RNASeqPairedEndAligner(
+            index,
+            transcriptome,
+            gtf,
+            maxReadSize,
+            confDiff,
+            maxHits,
+            maxDist,
+            numSeeds,
+            minSpacing,
+            maxSpacing,
+            adaptiveConfDiff);
+
+    SAMWriter *samWriter = this->samWriter;
+
+    // Keep grabbing ranges of the file and processing them.
+    PairedReadReader *reader = NULL;
+
+    _int64 rangeStart, rangeLength;
+    _int64 rangeStartTime;
+    int totalRanges = 0;
+    _int64 totalBytes = 0;
+    while (fileSplitter->getNextRange(&rangeStart, &rangeLength)) {
+        rangeStartTime = timeInMillis();
+        totalRanges++;
+        totalBytes += rangeLength;
+        if (NULL == reader) {
+            if (inputFileIsFASTQ) {
+                reader = PairedFASTQReader::create(inputFilename, fastqFile1, rangeStart, rangeLength, clipping);
+                if (NULL == reader) {
+                    fprintf(stderr, "Failed to create reader for '%s' or '%s'.\n", inputFilename, fastqFile1);
+                    exit(1);
+                }
+            } else {
+                reader = SAMReader::create(inputFilename, index->getGenome(), rangeStart, rangeLength, clipping);
+                if (NULL == reader) {
+                    fprintf(stderr, "Failed to create reader for '%s'.\n", inputFilename);
+                    exit(1);
+                }
+            }
+
+        } else {
+            reader->reinit(rangeStart, rangeLength);
+        }
+
+        // Align the reads.
+        Read read0(reader->getReaderToInitializeRead(0));
+        Read read1(reader->getReaderToInitializeRead(1));
+        _int64 readNum = 0;
+        while (reader->getNextReadPair(&read0,&read1)) {
+            if (1 != selectivity && GoodFastRandom(selectivity-1) != 0) {
+                //
+                // Skip this read.
+                //
+                continue;
+            }
+
+#ifdef PROFILE
+            readNum++;
+            bool record = (readNum % 1000 == 0);
+            _int64 start;
+            if (record) {
+                start = timeInNanos();
+            }
+#endif
+            
+            // Check that the two IDs form a pair; they will usually be foo/1 and foo/2 for some foo.
+            if (!ignoreMismatchedIDs && !readIdsMatch(&read0, &read1)) {
+                fprintf(stderr, "Unmatched read IDs %.*s and %.*s\n",
+                    read0.getIdLength(), read0.getId(), read1.getIdLength(), read1.getId());
+                exit(1);
+            }
+
+            stats->totalReads += 2;
+
+            // Skip the pair if there are too many Ns or 2s.
+            int maxDist = this->maxDist;
+            bool useful0 = read0.getDataLength() >= 50 && (int)read0.countOfNs() <= maxDist;
+            bool useful1 = read1.getDataLength() >= 50 && (int)read1.countOfNs() <= maxDist;
+            if (!useful0 && !useful1) {
+                PairedAlignmentResult result;
+                result.isTranscriptome[0] = false;
+                result.isTranscriptome[1] = false;
+                result.status[0] = NotFound;
+                result.status[1] = NotFound;
+                result.location[0] = 0xFFFFFFFF;
+                result.location[1] = 0xFFFFFFFF;
+                if (samWriter != NULL && (options->passFilter(&read0, result.status[0]) || options->passFilter(&read1, result.status[1]))) {
+                    samWriter->writePair(&read0, &read1, &result);
+                }
+                continue;
+            } else {
+                // Here one the reads might still be hopeless, but maybe we can align the other.
+                stats->usefulReads += (useful0 && useful1) ? 2 : 1;
+            }
+
+            PairedAlignmentResult result;
+            result.isTranscriptome[0] = false;
+            result.isTranscriptome[1] = false;
+            aligner->align(&read0, &read1, &result);
+            writePair(&read0, &read1, &result);
+            updateStats((RNASeqAlignerStats*) stats, &read0, &read1, &result);
+
+#ifdef PROFILE
+        if (record) {
+            _int64 end = timeInNanos();
+            printf("%d %lld %.*s %s %s %lld\n",
+                context->threadNum,
+                readNum,
+                read0.getIdLength(), read0.getId(),
+                AlignmentResultToString(result.status[0]),
+                AlignmentResultToString(result.status[1]),
+                end - start);
+        }
+#endif
+
+        }
+    }
+    //printf("Time in s: %lld: thread ran out of work.  Last range was %8lld bytes in %4lldms, starting at %10lld.  Total %4d ranges and %10lld bytes.\n",timeInMillis() / 1000, rangeLength, timeInMillis() - rangeStartTime, rangeStart, totalRanges, totalBytes);
+
+    delete aligner;
+    delete reader;
+}
+
+void RNASeqAlignerContext::writePair(Read* read0, Read* read1, PairedAlignmentResult* result)
+{
+    if (samWriter != NULL && (options->passFilter(read0, result->status[0]) || options->passFilter(read1, result->status[1]))) {
+        samWriter->writePair(read0, read1, result);
+    }
+}
+
+void RNASeqAlignerContext::updateStats(RNASeqAlignerStats* stats, Read* read0, Read* read1, PairedAlignmentResult* result)
+{
+    // Update stats
+    for (int r = 0; r < 2; r++) {
+        if (isOneLocation(result->status[r])) {
+            stats->singleHits++;
+            if (computeError) {
+                if (wgsimReadMisaligned((r == 0 ? read0 : read1), result->location[r], index, maxDist)) {
+                    stats->errors++;
+                }
+            }
+        } else if (result->status[r] == MultipleHits) {
+            stats->multiHits++;
+        } else {
+            _ASSERT(result->status[r] == NotFound);
+            stats->notFound++;
+        }
+    }
+    if (result->isRC[0] == result->isRC[1]) {
+        stats->sameComplement++;
+    }
+    if (isOneLocation(result->status[0]) && isOneLocation(result->status[1])) {
+        stats->incrementDistance(abs((int) (result->location[0] - result->location[1])));
+        stats->incrementScore(result->score[0], result->score[1]);
+    }
+}
