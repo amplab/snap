e5f31851be6be336de0a4ab12684ec0fee93392e
Bill Bolosky <bolosky@microsoft.com>
2015-01-27 17:21:02 -0800

    Windows build

--- SNAPLib/Compat.h
+++ SNAPLib/Compat.h
@@ -47,7 +47,7 @@ const _int16 INT16_MIN = MININT16;
 static const double LOG10 = log(10.0);
 inline double exp10(double x) { return exp(x * LOG10); }
 
-inline int round(double x) { return (int) (x + 0.5); }
+//inline int round(double x) { return (int) (x + 0.5); }
 
 const void* memmem(const void* data, const size_t dataLength, const void* pattern, const size_t patternLength);
 
