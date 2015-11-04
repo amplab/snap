ee3efdd08aa8ab404b009d95f7547b44d874f6db
Bill Bolosky <bolosky@microsoft.com>
2015-11-04 13:20:35 -0800

    1.0beta.22: Paired read matcher doesn't drop reads where both mates are unaligned in the input

--- SNAPLib/CommandProcessor.cpp
+++ SNAPLib/CommandProcessor.cpp
@@ -35,7 +35,7 @@ Pulled from the main program and expanded to handle daemon mode
 #include "Error.h"
 #include "Compat.h"
 
-const char *SNAP_VERSION = "1.0beta.21";
+const char *SNAP_VERSION = "1.0beta.22";
 
 static void usage()
 {
